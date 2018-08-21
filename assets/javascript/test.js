f18e6beda9c1709a02ca7bbc088bb045211edafe
ymah <yhamid1982@gmail.com>
2018-08-21 18:37:13 -0400

    copy of game.js made to test.js for refactoring

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -108,32 +108,32 @@ $(document).ready(function () {
   //   TEST.push(fighter);
   // }
 
-  let fighters = [
-    {
-      name: 'Justin Trudeau',
-      hp: rand(160, 100),
-      ap: rand(10, 2),
-      img: 'assets/images/trudeau.jpg'
-    },
-    {
-      name: 'Barrack Obama',
-      hp: rand(200, 100),
-      ap: rand(10, 2),
-      img: 'assets/images/obama.jpg'
-    },
-    {
-      name: 'Emmanuel Macron',
-      hp: rand(140, 100),
-      ap: rand(10, 2),
-      img: 'assets/images/macron.jpg'
-    },
-    {
-      name: 'Angela Merkel',
-      hp: rand(180, 100),
-      ap: rand(10, 2),
-      img: 'assets/images/merkel.jpg'
-    }
-  ];
+  // let fighters = [
+  //   {
+  //     name: 'Justin Trudeau',
+  //     hp: rand(160, 100),
+  //     ap: rand(10, 2),
+  //     img: 'assets/images/trudeau.jpg'
+  //   },
+  //   {
+  //     name: 'Barrack Obama',
+  //     hp: rand(200, 100),
+  //     ap: rand(10, 2),
+  //     img: 'assets/images/obama.jpg'
+  //   },
+  //   {
+  //     name: 'Emmanuel Macron',
+  //     hp: rand(140, 100),
+  //     ap: rand(10, 2),
+  //     img: 'assets/images/macron.jpg'
+  //   },
+  //   {
+  //     name: 'Angela Merkel',
+  //     hp: rand(180, 100),
+  //     ap: rand(10, 2),
+  //     img: 'assets/images/merkel.jpg'
+  //   }
+  // ];
 
   let trudeau = fighters[0];
   let obama = fighters[1];
@@ -322,64 +322,3 @@ $(document).ready(function () {
   $(document).on('click', '.char', selector)
     .on('click', '#restart', initialize);
 })
-
-/*
-
-The player must then defeat all of the remaining fighters.Enemies should be moved to a different area of the screen.
-
-The player chooses an opponent by clicking on an enemy's picture.
-
-Once the player selects an opponent, that enemy is moved to a defender area.
-
-The player will now be able to click the attack button.
-
-
-Whenever the player clicks attack, their character damages the defender.The opponent will lose HP(health points).These points are displayed at the bottom of the defender's picture. 
-The opponent character will instantly counter the attack.When that happens, the player's character will lose some of their HP. These points are shown at the bottom of the player character's picture.
-
-
-
-
-
-The player will keep hitting the attack button in an effort to defeat their opponent.
-
-
-
-When the defender's HP is reduced to zero or below, remove the enemy from the defender area. The player character can now choose a new opponent.
-
-
-
-The player victories the game by defeating all enemy enemies.The player loses the game the game if their character's HP falls to zero or below.
-
-
-
-Option 2 Game design notes
-
-
-Each character in the game has 3 attributes: Health Points, Attack Power and Counter Attack Power.
-
-Each time the player attacks, their character's Attack Power increases by its base Attack Power. 
-
-
-For example, if the base Attack Power is 6, each attack will increase the Attack Power by 6(12, 18, 24, 30 and so on).
-
-
-
-The enemy character only has Counter Attack Power.
-
-
-Unlike the player's Attack Points, Counter Attack Power never changes.
-
-
-The Health Points, Attack Power and Counter Attack Power of each character must differ.
-
-No enemies in the game can heal or recover Health Points.
-
-
-A winning player must pick their enemies wisely by first fighting an enemy with low Counter Attack Power.This will allow them to grind Attack Power and to take on enemies before they lose all of their Health Points.Healing options would mess with this dynamic.
-
-
-Your players should be able to win and lose the game no matter what character they choose.The challenge should come from picking the right enemies, not choosing the strongest player.*/
-
-
-
